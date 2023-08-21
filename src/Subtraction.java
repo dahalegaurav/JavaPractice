2d3b31e8ef642e82871a0445af57e85519972ad8
DESKTOP-LF5BVKA\Gaurav <gaurav.dahale@goionet.in>
2023-08-21 23:09:02 +0530

    Substraction and Division Programe

---
+++ src/Subtraction.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        int substraction = a - b;
+        System.out.println("Static Value Output :"+substraction);
+        dynamicValue();
+    }
+
+    private static void dynamicValue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Value : ");
+        int dynamicA = scanner.nextInt();
+        System.out.println("Enter Secound Value : ");
+        int dynamicB = scanner.nextInt();
+        int dynamicOutput = dynamicA-dynamicB;
+        System.out.println("Dynamic Value Output : " +dynamicOutput);
+
+    }
+}
