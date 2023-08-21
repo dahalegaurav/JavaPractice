beef761a905f274ad940269a9f5d597723d5ebb8
DESKTOP-LF5BVKA\Gaurav <gaurav.dahale@goionet.in>
2023-08-21 22:53:43 +0530

    Addition of two numbers with static value or dynamic value Programe

--- src/Addition.java
+++ src/Addition.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Addition {
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 22;
+        int sum = a+b;
+        System.out.println("Sum Of Static Value"+sum);
+        dynamicvaue();
+    }
+
+    private static void dynamicvaue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Value Of A");
+        int dynamicA = scanner.nextInt();
+        System.out.println("Enter Value Of B");
+        int dynamicB = scanner.nextInt();
+        int dynamicSum = dynamicA+dynamicB;
+        System.out.println("Sum Of Dynamic Value"+dynamicSum);
+    }
 }
