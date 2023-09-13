90b9203e643c99144d8205591cd2aebeca1f9524
DESKTOP-LF5BVKA\Gaurav <gaurav.dahale@goionet.in>
2023-09-13 21:42:23 +0530

    Minimum And Maximum Number Finding

---
+++ src/FindMaximumAndMinimun.java
@@ -0,0 +1,11 @@
+public class FindMaximumAndMinimun {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+        int c = Math.max(a,b);
+        int d = Math.min(a,b);
+        System.out.println("Maximum Number Is: "+c);
+        System.out.println("Minimum Number Is: "+d);
+
+    }
+}
