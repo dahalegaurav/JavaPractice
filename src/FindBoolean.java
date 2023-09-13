4795523d6b71bad3e89c059f835de7f8d95f440a
DESKTOP-LF5BVKA\Gaurav <gaurav.dahale@goionet.in>
2023-09-13 22:33:39 +0530

    Find Boolean Number

---
+++ src/FindBoolean.java
@@ -0,0 +1,11 @@
+public class FindBoolean {
+    public static void main(String[] args) {
+        Boolean a = true;
+        Boolean b = false;
+        System.out.println(a);
+        System.out.println(b);
+        int x = 10;
+        System.out.println(x == 10);
+        System.out.println(x == 7);
+    }
+}
